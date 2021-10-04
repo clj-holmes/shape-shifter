b813e56cd6d4faa3ef7deb490cb936171db246fe
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-10-03 22:42:55 -0300

    everything private

--- src/shape_shifter/core.clj
+++ src/shape_shifter/core.clj
@@ -19,16 +19,16 @@
    "$vector"  `vector?
    "$regex"   `s/regex?})
 
-(defmulti transform (fn [[key _]]  key))
+(defmulti ^:private transform (fn [[key _]]  key))
 
-(defn recursively-transform [coll-of-specs value]
+(defn ^:private recursively-transform [coll-of-specs value]
   (let [random-key-name (utils/random-keyword 5)
         transformed-value (transform value)]
     (if transformed-value
       (conj coll-of-specs random-key-name transformed-value)
       coll-of-specs)))
 
-(defn pattern->collection [patterns kind]
+(defn ^:private pattern->collection [patterns kind]
   (->> patterns
        (reduce recursively-transform [])
        (cons `s/cat)
@@ -37,29 +37,34 @@
        list
        (cons `s/spec)))
 
-(defmethod transform :list [[_ & values]]
+(defmethod ^:private transform :list [[_ & values]]
   (pattern->collection values `list?))
 
-(defmethod transform :vector [[_ & values]]
+(defmethod ^:private transform :vector [[_ & values]]
   (pattern->collection values `vector?))
 
-(defmethod transform :number [[_ value]]
+(defmethod ^:private transform :number [[_ value]]
   `#{(edn/read-string ~value)})
 
-(defmethod transform :symbol [[_ value]]
+(defmethod ^:private transform :symbol [[_ value]]
   (or (get wildcards value)
       `#{(symbol ~value)}))
 
-(defmethod transform :string [[_ value]]
+(defmethod ^:private transform :string [[_ value]]
   `#{~(string/replace value #"\"" "")})
 
-(defmethod transform :regex [[_ value]]
+(defmethod ^:private transform :regex [[_ value]]
   `#{~(format "#%s" value)})
 
-(defmethod transform :whitespace [[_ _]] nil)
+(defmethod ^:private transform :whitespace [[_ _]] nil)
 
 (defn pattern->spec [pattern]
-  (-> pattern (parcera/ast :unhide :literals) rest first transform eval))
+  (-> pattern
+      (parcera/ast :unhide :literals)
+      rest
+      first
+      transform
+      eval))
 
 (comment
   (-> "(+ 1 1)" pattern->spec (s/valid? '(+ 1 1))))
