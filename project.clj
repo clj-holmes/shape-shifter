55c82fd9ef9fa886115e3ec801698454cfea0281
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-10-05 16:50:30 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.clj-holmes/shape-shifter "0.1.2"
+(defproject org.clojars.clj-holmes/shape-shifter "0.1.3"
   :url "https://github.com/clj-holmes/shape-shifter"
   :description "Transforms a string pattern into clojure spec."
   :scm {:name "git"
-        :url  "https://github.com/clj-holmes/clj-holmes"}
+        :url  "https://github.com/clj-holmes/shape-shifter"}
   :license {:name "Eclipse Public License 1.0"
             :url  "http://opensource.org/licenses/eclipse-1.0.php"}
 
