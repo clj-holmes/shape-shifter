c66437ad4b5df9f50b79cffb42a61d6b0586251e
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-12-16 11:05:02 -0300

    Bump n plugins (#9)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.clj-holmes/shape-shifter "0.3.6"
+(defproject org.clojars.clj-holmes/shape-shifter "0.3.7"
   :url "https://github.com/clj-holmes/shape-shifter"
   :description "Transforms a string pattern into clojure spec."
   :scm {:name "git"
@@ -6,11 +6,13 @@
   :license {:name "Eclipse Public License 1.0"
             :url  "http://opensource.org/licenses/eclipse-1.0.php"}
 
-  :plugins [[lein-ancient "0.6.15"]
-            [lein-cljfmt "0.6.4"]
+  :plugins [[lein-ancient "0.7.0"]
+            [lein-cljfmt "0.8.0"]
             [lein-shell "0.5.0"]
+            [lein-kibit "0.1.8"]
+            [lein-cloverage "1.2.2"]
             [lein-nsorg "0.3.0"]
-            [jonase/eastwood "0.3.10"]]
+            [jonase/eastwood "0.9.9"]]
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
@@ -22,7 +24,7 @@
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [org.babashka/sci "0.2.7"]
                  [carocad/parcera "0.11.6"]
-                 [org.antlr/antlr4-runtime "4.7.1"]]
+                 [org.antlr/antlr4-runtime "4.9.3"]]
 
   :profiles {:uberjar {:global-vars {*assert* false}
                        :jvm-opts    ["-Dclojure.compiler.direct-linking=true"
@@ -31,8 +33,8 @@
                        :aot         :all}}
   :main        shape-shifter.core
 
-  :aliases {"lint"            ["do" ["cljfmt" "check"] ["nsorg"] ["eastwood" "{:namespaces [:source-paths]}"]]
-            "lint-fix"        ["do" ["cljfmt" "fix"] ["nsorg" "--replace"]]
+  :aliases {"lint"            ["do" ["cljfmt" "check"] ["kibit"] ["nsorg"] ["eastwood" "{:namespaces [:source-paths]}"]]
+            "lint-fix"        ["do" ["cljfmt" "fix"] ["kibit" "--replace"] ["nsorg" "--replace"]]
             "native"          ["shell" "native-image" "--report-unsupported-elements-at-runtime"
                                "--initialize-at-build-time" "--no-fallback" "-H:+ReportExceptionStackTraces"
                                "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
